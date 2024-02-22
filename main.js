f226fa5341cf91420966a155a4a8eb7c4d7a637d
abhinav80066 <abhinav.chaudhary800@gmail.com>
2024-02-22 23:09:34 +0530

    Greeting section and image section is completed

--- main.js
+++ main.js
@@ -6,21 +6,97 @@ document.querySelector("#close-nav-menu").addEventListener("click", function(){d
 
 //Greeting Section
 
-const greetingText = "Good Morning";
+function celsiusToFahr(temp){
+    let fahr = (temp * 9/5) + 32;
+    return fahr;
+}
+
+let currentHour = new Date().getHours();
+let greetingText;
+if (currentHour < 12) {
+    greetingText = "Good Morning";
+} else if (currentHour < 19){
+    greetingText = "Good Afternoon";
+} else if (currentHour < 24){
+    greetingText = "Good Evening";
+} else {
+    greetingText = "Welcome";
+}
+
 const weatherCondition = "sunny";
 const userLocation = " Mumbai";
-let temp = 22.378;
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and its ${temp.toFixed(1)}C outside.`;
+let temp = 25;
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and its ${temp.toFixed(1)}C outside.`;
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and its ${celsiusToFahr(temp).toFixed(1)}F outside.`;
 
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText;
+document.querySelector("p#weather").innerHTML = celsiusText;
 
 document.querySelector(".weather-group").addEventListener("click", function(e){
    if(e.target.id == "celsius") {
-    console.log("Celsius is clicked")
+    document.querySelector("p#weather").innerHTML = celsiusText;
    }else if (e.target.id == "fahr") {
-    console.log("Fahr is clicked")
+    document.querySelector("p#weather").innerHTML = fahrText;
    }
-    
 
-})
+});
+
+//Local time section
+
+setInterval(function(){
+    let localTime = new Date();
+    let hour = localTime.getHours();
+    if(hour > 12){
+        hour = hour - 12;
+    }
+    document.querySelector("span[data-time=hours]").textContent = hour.toString().padStart(2,"0");
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0"); 
+},1000);
+
+//Gallery Section
+
+const galleryImages = [
+    {
+        src:"./assets/gallery/image1.jpg",
+        alt:"Thumbnail Image 1"
+    },
+    {
+        src:"./assets/gallery/image2.jpg",
+        alt:"Thumbnail Image 2"
+    },
+    {
+        src:"./assets/gallery/image3.jpg",
+        alt:"Thumbnail Image 3"
+    }
+]
+// for (let i in galleryImages){
+//     console.log(galleryImages[i]);
+// }
+let mainImage = document.querySelector("#gallery > img");
+let thumbnails = document.querySelector("#gallery .thumbnails");
+
+mainImage.src = galleryImages[0].src;
+mainImage.alt = galleryImages[0].alt;
+
+galleryImages.forEach(function(image,index){
+    let thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumb.alt = image.alt;
+    thumb.dataset.arrayIndex = index;
+    thumb.dataset.selected = index === 0 ? true : false;
+
+    thumb.addEventListener("click", function(e){
+        let selectedIndex = e.target.dataset.arrayIndex;
+        let selectedimage = galleryImages[selectedIndex];
+        mainImage.src = selectedimage.src;
+        mainImage.alt = selectedimage.alt;
+        thumbnails.querySelectorAll("img").forEach(function(img){
+            img.dataset.selected = false;
+        });
+        e.target.dataset.selected = true;
+    });
+
+    thumbnails.appendChild(thumb);
+});
+
